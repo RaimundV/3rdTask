e3a75fc0b7ba8904e1fea96363723ae553f8d9c7
Raimund Vysockij <rajmund.wysocki@gmail.com>
2018-02-13 10:54:45 +0200

    did the 1st task

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
-
+#include <string>
+#include <iomanip>
 
 int main()
 {
+    const double proc1 = 0.4;
+    const double proc2 = 0.6;
+    std::string name;
+    std::string surname;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Enter your surname: ";
+    std::cin >> surname;
+    int quantity = 0;
+    std::cout << "Enter the quantity of the students homework: ";
+    std::cin >> quantity;
+    double Hgrade[quantity];
+    for(int i = 0; i < quantity; i++)
+    {
+        std::cout << "Enter homework nr. " << i + 1 << ": ";
+        std::cin >> Hgrade[i];
+    }
+    double Egrade;
+    std::cout << "Enter exam grade: ";
+    std::cin >> Egrade;
+    int option = 0;
+    double vidurkisg = 0;
+    double vidurkis = 0;
+    while(option == 0)
+    {
+        std::cout << "1. Count as vidurkis\n 2. Count as mediana\n Enter option: ";
+        std::cin >> option;
+        if(option == 1)
+        {
+            for(int i = 0; i < quantity; i++)
+            {
+                vidurkisg += Hgrade[i];
+            }
+            vidurkisg /= quantity;
+            vidurkis = proc1 * vidurkisg + proc2 * Egrade;
+        }
+        if(option == 2)
+        {
+           // vidurkisg = median(Hgrade);
+        }
+        if(option < 0 || option > 2)
+        {
+            std::cout << "There is no such option please choose again." << std::endl;
+            option = 0;
+        }
+    }
+    std::cout << std::setprecision(3) << vidurkis;
+
     return 0;
 }
