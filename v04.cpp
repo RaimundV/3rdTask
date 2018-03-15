69245a4b3f78fc1dd98de3dc49d958c830bbea4d
Raimund Vysockij <rajmund.wysocki@gmail.com>
2018-03-15 15:07:46 +0200

    Made it a bit faster

--- v04.cpp
+++ v04.cpp
@@ -1,33 +1,31 @@
-//#include "fun.h"
 #include <iostream>
 #include <fstream>
 #include <chrono>
-#include <ratio>
 #include <ctime>
 #include <random>
-#include <cstdio>
 #include <string>
-#include <cstdlib>
 #include <sstream>
 #include <algorithm>
+#include <iomanip>
 
 struct Student
 {
     std::string name;
     std::string surname;
-    std::vector<double> Hgrade;
-    std::vector<double> Egrade;
+    std::vector<int> Hgrade;
+    int Egrade;
     std::string type;
-    bool operator()(const Student& lhs, const Student& rhs) const { lhs.type > rhs.type; }
+    //bool operator()(const Student& lhs, const Student& rhs) const { lhs.type > rhs.type; }
 };
 
 bool compareByLength(const Student &a, const Student &b)
 {
-    return a.type.size() < b.type.size();
+    return a.type < b.type;
 }
 
-void Random(int quantity)
+void Random(size_t quantity, std::string k)
 {
+    using namespace std::chrono;
     std::vector<Student> s;
     std::random_device rseed;
     std::mt19937 rgen(rseed()*static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().
@@ -35,7 +33,8 @@ void Random(int quantity)
     std::uniform_int_distribution<int> idist(1,10); // [1,10]
     std::ofstream file;
     double vidurkis;
-    for (int i = 0; i < quantity; i++)
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    for (size_t i = 0; i < quantity; i++)
     {
         s.push_back(Student());
         s[i].name = "vard";
@@ -44,49 +43,66 @@ void Random(int quantity)
         convert << i;
         s[i].name    += convert.str();
         s[i].surname += convert.str();
-        //file << name << " " << surname << " ";
         vidurkis = 0;
-        for(int j = 0; j < 5; j++)
+        for(size_t j = 0; j < 5; j++)
         {
             s[i].Hgrade.push_back(idist(rgen));
             vidurkis += s[i].Hgrade[j];
         }
-        if((vidurkis / 5.0) >= 6.0)
-        {
-            s[i].type = "Winner";
-        }
-        else
-        {
-            s[i].type = "Loser";
-        }
-        s[i].Egrade.push_back(idist(rgen));
+
+        //s[i].type = vidurkis / 5.0 >= 6.0 ? "Winner" : "Loser";
+        s[i].Egrade = idist(rgen);
     }
-    std::sort(s.begin(), s.end(), compareByLength);
-    file.open("wow.txt");
-    for(int i = 0; i < quantity; i++)
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double, std::milli> time_span = t2 - t1;
+    std::cout << "the function took: " << std::setprecision(5) << time_span.count()/1000 << " seconds.";
+    file.open(k);
+    for(size_t i = 0; i < quantity; i++)
     {
         file << s[i].name << " " << s[i].surname << " ";
-        for(int j = 0; j < 5; j++)
+        for(size_t j = 0; j < 5; j++)
         {
             file << s[i].Hgrade[j] << " ";
         }
-        file << s[i].Egrade[0] << " ";
-        file << s[i].type << std::endl;
+        file << s[i].Egrade << std::endl;
     }
     file.close();
 }
 
+
 int main()
 {
     using namespace std::chrono;
-    int quantity;
-    std::cout << "Enter amount of students: ";
-    std::cin >> quantity;
+    size_t quantity = 0;
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    Random(quantity);
+    Random(10, "wow1.txt");
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
     duration<double, std::milli> time_span = t2 - t1;
-    std::cout << "It took me " << time_span.count()/1000 << " seconds.";
+    std::cout << "wow1.txt generated in: " << time_span.count()/1000 << " seconds." << std::endl;
+
+    t1 = high_resolution_clock::now();
+    Random(100, "wow2.txt");
+    t2 = high_resolution_clock::now();
+    time_span = t2 - t1;
+    std::cout << "wow2.txt generated in: " << time_span.count()/1000 << " seconds." << std::endl;
+
+    t1 = high_resolution_clock::now();
+    Random(1000, "wow3.txt");
+    t2 = high_resolution_clock::now();
+    time_span = t2 - t1;
+    std::cout << "wow3.txt generated in: " << time_span.count()/1000 << " seconds." << std::endl;
+
+    t1 = high_resolution_clock::now();
+    Random(10000, "wow4.txt");
+    t2 = high_resolution_clock::now();
+    time_span = t2 - t1;
+    std::cout << "wow4.txt generated in: " << time_span.count()/1000 << " seconds." << std::endl;
+
+    t1 = high_resolution_clock::now();
+    Random(100000, "wow5.txt");
+    t2 = high_resolution_clock::now();
+    time_span = t2 - t1;
+    std::cout << "wow5.txt generated in: " << time_span.count()/1000 << " seconds." << std::endl;
     return 0;
 }
 
