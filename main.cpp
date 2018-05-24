03216920eb1a4e3a9b7bbadce86cef4470450917
Raimund Vysockij <rajmund.wysocki@gmail.com>
2018-05-25 00:19:00 +0300

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -10,31 +10,13 @@ int main(int argc, char* argv[])
     PerformanceCheck();
     std::cout << "check container performance" << std::endl;
     ContainerPerformanceCheck(5); // Change to 6 so the programme runs the containers without algorithms
-    Catch::Session session; // There must be exactly one instance
 
-    int height = 0; // Some user variable you want to be able to set
+    Catch::Session session;
 
-    // Build a new parser on top of Catch's
-    using namespace Catch::clara;
-    auto cli
-            = session.cli() // Get Catch's composite command line parser
-              | Opt( height, "height" ) // bind variable to a new option, with a hint string
-              ["-g"]["--height"]    // the option names it will respond to
-                      ("how high?");        // description string for the help output
-
-    // Now pass the new composite back to Catch so it uses that
-    session.cli( cli );
-
-    // Let Catch (using Clara) parse the command line
     int returnCode = session.applyCommandLine( argc, argv );
-    if( returnCode != 0 ) // Indicates a command line error
+    if( returnCode != 0 )
         return returnCode;
 
-    // if set on the command line then 'height' is now set at this point
-    if( height > 0 )
-        std::cout << "height: " << height << std::endl;
-
     return session.run();
-    //return 0;
 }
 
